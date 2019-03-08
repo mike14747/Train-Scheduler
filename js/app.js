85c1b5f287bb57f0f00e79d8f84488be9a993bdc
mike4747 <mike4747@oh.rr.com>
2019-03-07 21:55:46 -0500

    consolidated some code and got rid of the logout button

--- js/app.js
+++ js/app.js
@@ -137,7 +137,3 @@ $("#submit_train").on("click", function () {
         setTimeout(function () { $("#train_submitted").addClass("display_none"); }, 3000);
     }
 });
-
-function logOut() {
-    firebase.auth().signOut();
-}
